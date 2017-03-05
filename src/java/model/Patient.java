9a702fc88d50e0c524a6bbac51b17fbf2eab843b
georgy varghese kurian <c0687988@mylambton.ca>
2017-03-05 04:31:15 -0500

    Added the file

---
+++ src/java/model/Patient.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Georgi
+ */
+public class Patient {
+    private String username;
+    private String patientId;
+    private String name;
+    private int age;
+
+    public Patient(String username, String patientId, String name, int age) {
+        this.username = username;
+        this.patientId = patientId;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
